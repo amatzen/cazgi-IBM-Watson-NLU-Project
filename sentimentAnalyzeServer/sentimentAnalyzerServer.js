2b69bc27afc12e1716991d2546cbaaed6b084260
Alexander Matzen <alexaner@amatzen.dk>
2021-05-02 09:52:00 +0000

    First step

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -6,9 +6,27 @@ app.use(express.static('client'))
 const cors_app = require('cors');
 app.use(cors_app());
 
+const getNLUInstance = () => {
+    const api_key = process.env.API_KEY;
+    const api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const nlu = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key
+        }),
+        serviceUrl: api_url,
+    });
+
+    return nlu;
+}
+
 app.get("/",(req,res)=>{
     res.render('index.html');
-  });
+});
 
 app.get("/url/emotion", (req,res) => {
 
